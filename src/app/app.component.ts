8aa7b71e2f934e1e570c33a7c19cbce6b48bd227
Pető József <petoojozsef@gmail.com>
2023-07-10 15:29:52 +0200

    Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, AfterViewInit } from '@angular/core';
+import { AuthService } from './nyilvanos/service/auth.service';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit {
+
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  cim = 'bowling';
+  beJelolve : any;
+  bejelentkezetFelhasznalo?: firebase.default.User | null;
+
+  ngAfterViewInit(): void {
+    this.authService.felhasznaloBejelentkezve().subscribe(user => {
+      this.bejelentkezetFelhasznalo = user;
+      localStorage.setItem("felhasznalo", JSON.stringify(this.bejelentkezetFelhasznalo));
+    }, error => {
+      console.error(error);
+      localStorage.setItem("felhasznalo", JSON.stringify('null'));
+    });
+  }
+
+  alapAllapot() {
+    this.beJelolve = document.getElementById('csukas');
+    this.beJelolve.checked = false;
+
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
+
+  kijelentkezes() {
+    this.authService.kijelentkezes().then(() =>{
+      console.log("Sikeres kijelentkezes!");
+      localStorage.removeItem('felhasznalo');
+      this.router.navigateByUrl('/kezdolap');
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
+  oldalsavKatt(oldalsav: MatSidenav) {
+    oldalsav.toggle();
+  }
+
+  oldalsavBezaras(event: any, oldalsav: MatSidenav) {
+    if(event === true) {
+      oldalsav.close();
+    }
+  }
+}
